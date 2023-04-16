b59bbca872a75938c2d21dc8ec248420c81f448d
huseyin5 <395273@ogr.ktu.edu.tr>
2023-04-16 13:44:01 +0300

    fixed update

--- export/Semalar.py
+++ export/Semalar.py
@@ -7,13 +7,18 @@ from .ma import ma
 class EgitmenSema(ma.SQLAlchemyAutoSchema):
     class Meta:
         model = Egitmen
+        load_instance=True
 
 
 class SinifSema(ma.SQLAlchemyAutoSchema):
     class Meta:
         model = Sinif
+        load_instance=True
+
 
 
 class DersSema(ma.SQLAlchemyAutoSchema):
     class Meta:
         model = Ders
+        load_instance=True
+
