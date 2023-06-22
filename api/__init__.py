692ab7055799a764ed8b4e7105988c41e7a37c84
huseyin5 <395273@ogr.ktu.edu.tr>
2023-06-22 21:01:39 +0300

    fixed update

--- api/__init__.py
+++ api/__init__.py
@@ -1,12 +1,16 @@
 from flask import Blueprint
+from flask_cors import CORS
+
 from api.genel_api import genel_api_olustur
 from db import Egitmen, Ders, Sinif
 from export.Semalar import EgitmenSema, DersSema, SinifSema
 
 v1 = Blueprint('v1', __name__)
-v1.register_blueprint(genel_api_olustur("egitmen", Egitmen, EgitmenSema), url_prefix="egitmen")
+v1.register_blueprint(genel_api_olustur("egitmen", Egitmen, EgitmenSema),url_prefix="/egitmen")
 v1.register_blueprint(genel_api_olustur("ders", Ders, DersSema), url_prefix="/ders")
 v1.register_blueprint(genel_api_olustur("sinif", Sinif, SinifSema), url_prefix="/sinif")
 
 api = Blueprint('api', __name__)
 api.register_blueprint(v1, url_prefix="/v1")
+
+CORS(api)
--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import os
 from dotenv import load_dotenv
 from flask import Flask
 from flask_migrate import Migrate
+from flask_cors import CORS
 from api import api
 from db import db
 from export import ma
@@ -14,14 +15,9 @@ app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DB_URL")
 db.init_app(app)
 ma.init_app(app)
 Migrate(app, db)
+CORS(app, supports_credentials=True, resources="*", origins=["http://localhost:5000", "http://localhost:5173"])
 
 app.register_blueprint(api, url_prefix="/api")
-
-
-@app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
-
 
 if __name__ == '__main__':
     app.run()
